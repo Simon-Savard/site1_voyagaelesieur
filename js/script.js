3e162fe915200d9218299c1d16ea9e9823faa7ec
Simon-Savard <simonsavardb@gmail.com>
2025-01-29 14:48:39 -0500

    js done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-baller 
+const btnPlus = document.querySelector(".js-plus");
+const contTransport = document.querySelector(".conteneur-transport");
+
+btnPlus.addEventListener("click", toggleTransport);
+
+function toggleTransport(){
+    contTransport.classList.toggle("invisible");
+}
